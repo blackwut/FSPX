6e03016f4501dab19cc292760b238e3a6ef84d51
blackwut <albertoottimo@gmail.com>
2023-09-14 17:51:46 +0200

    [Update] renamed MemoryReader and MemoryWriter into StreamGenerator and StreamDrainer

--- include/fspx_host.hpp
+++ include/fspx_host.hpp
@@ -3,7 +3,10 @@
 
 #include "host/utils.hpp"
 #include "host/ocl.hpp"
-#include "host/memory_reader.hpp"
-#include "host/memory_writer.hpp"
+#include "host/stream_generator.hpp"
+#include "host/stream_drainer.hpp"
+#include "host/metric/metric.hpp"
+#include "host/metric/sampler.hpp"
+#include "host/metric/metric_group.hpp"
 
 #endif // __FSPX_HOST__
