34c0f6adc7c6f888e9b52ef66fbabd545bcf111b
blackwut <albertoottimo@gmail.com>
2023-10-30 10:52:48 +0100

    Added common, tumbling and sliding windows

--- include/fspx.hpp
+++ include/fspx.hpp
@@ -1,8 +1,12 @@
 #ifndef __FSPX_HPP__
 #define __FSPX_HPP__
 
+#include "common.hpp"
 #include "connectors/connectors.hpp"
 #include "datastructures/typehandler.hpp"
+#include "datastructures/aggregate_operators.hpp"
+#include "datastructures/tumbling_window.hpp"
+#include "datastructures/sliding_window.hpp"
 #include "operators/operators.hpp"
 #include "streams/stream.hpp"
 
