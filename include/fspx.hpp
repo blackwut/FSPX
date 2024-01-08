6cfd1163186a2f89dd980615932cd5337260cd5e
blackwut <albertoottimo@gmail.com>
2024-01-08 16:47:41 +0100

    [Clean] cleaning

--- include/fspx.hpp
+++ include/fspx.hpp
@@ -2,12 +2,12 @@
 #define __FSPX_HPP__
 
 #include "common.hpp"
-#include "connectors/connectors.hpp"
+#include "streams/stream.hpp"
 #include "datastructures/typehandler.hpp"
 #include "datastructures/aggregate_operators.hpp"
 #include "datastructures/tumbling_window.hpp"
 #include "datastructures/sliding_window.hpp"
+#include "connectors/connectors.hpp"
 #include "operators/operators.hpp"
-#include "streams/stream.hpp"
 
 #endif // __FSPX_HPP__
