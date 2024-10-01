c49670558379d32c3caf669e94f087783bd6f0eb
blackwut <albertoottimo@gmail.com>
2024-10-01 13:05:15 +0200

    Commenting print_debug function

--- include/connectors/connectors.hpp
+++ include/connectors/connectors.hpp
@@ -1,20 +1,20 @@
 #ifndef __CONNECTORS_HPP__
 #define __CONNECTORS_HPP__
 
-#if defined(__DEBUG__CONNECTORS__)
-#include <typeinfo>
-template <typename T>
-void print_debug(const std::string name, const std::string message, T t) {
+// #if defined(__DEBUG__CONNECTORS__)
+// #include <typeinfo>
+// template <typename T>
+// void print_debug(const std::string name, const std::string message, T t) {
 
-    std::cout << "[" name << "] " << message << " ";
+//     std::cout << "[" name << "] " << message << " ";
 
-    if constexpr (!std::is_member_function_pointer<decltype(&T::print_debug())>::value) {
-        std::cout << typeid(t).name() << " has not print_debug() member function!" << std::endl;
-    } else {
-        t.print_debug();
-    }
-}
-#endif // __DEBUG__CONNECTORS__
+//     if constexpr (!std::is_member_function_pointer<decltype(&T::print_debug())>::value) {
+//         std::cout << typeid(t).name() << " has not print_debug() member function!" << std::endl;
+//     } else {
+//         t.print_debug();
+//     }
+// }
+// #endif // __DEBUG__CONNECTORS__
 
 #include "generic.hpp"
 #include "memory.hpp"
